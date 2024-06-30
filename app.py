c47bc873e424d1f4e5c8ea69ba8956eab9a9508b
Amri Reza <amrireza97@gmail.com>
2024-06-30 08:35:55 +0700

    refactor: app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 # Load model
 model = joblib.load('model.pkl')  # Sesuaikan dengan nama dan lokasi file model Anda
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,11 +19,13 @@ def predict():
         # Ambil nilai input dari form
         input_data = [float(x) for x in request.form.values()]
         input_array = np.array(input_data).reshape(1, -1)
-        
+
         # Lakukan prediksi menggunakan model
         prediction = model.predict(input_array)
-        
-        return render_template('index.html', features_text=f"{input_data}", prediction_text=f'Prediksi Harga Rumah: $ {prediction[0]:,.2f}')
+
+        return render_template('index.html', features_text=f"{input_data}",
+                               prediction_text=f'Prediksi Harga Rumah: $ {prediction[0]:,.2f}')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
